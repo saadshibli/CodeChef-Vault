d3ab11e1d51dff343b4ec31618df4ebb26ce1384
Saad Mohammad Shibli <saadshibli27@gmail.com>
2025-08-28 02:23:09 +0530

    Add Q.Selling Coins

---
+++ Contest/START/201/SELLINGCOINS.cpp
@@ -0,0 +1,12 @@
+// Source : https://www.codechef.com/START201D/problems/SELLCOIN
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a, b;
+    cin>>a>>b;
+    cout<<(a*1)+(b*2)<<endl;
+    return 0;
+}
